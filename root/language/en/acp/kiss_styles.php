72d872a93bef2586ebb0ca1d7595a2bcb23fe45c
Michael O'Toole <phpbbireland@gmail.com>
2012-05-23 00:26:21 +0200

    Update root/language/en/acp/kiss_styles.php

--- root/language/en/acp/kiss_styles.php
+++ root/language/en/acp/kiss_styles.php
@@ -37,13 +37,13 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 
-	'IMG_NEWS_READ'					=> 'News',
-	'IMG_NEWS_READ_MINE'			=> 'News posted to',
-	'IMG_NEWS_READ_LOCKED'			=> 'News locked',
-	'IMG_NEWS_READ_LOCKED_MINE'		=> 'News locked posted to',
-	'IMG_NEWS_UNREAD'				=> 'News new posts',
-	'IMG_NEWS_UNREAD_MINE'			=> 'News posted to new',
-	'IMG_NEWS_UNREAD_LOCKED'		=> 'News locked new post',
+	'IMG_NEWS_READ'			=> 'News',
+	'IMG_NEWS_READ_MINE'		=> 'News posted to',
+	'IMG_NEWS_READ_LOCKED'		=> 'News locked',
+	'IMG_NEWS_READ_LOCKED_MINE'	=> 'News locked posted to',
+	'IMG_NEWS_UNREAD'		=> 'News new posts',
+	'IMG_NEWS_UNREAD_MINE'		=> 'News posted to new',
+	'IMG_NEWS_UNREAD_LOCKED'	=> 'News locked new post',
 	'IMG_NEWS_UNREAD_LOCKED_MINE'	=> 'News locked posted to new',
 
 ));
