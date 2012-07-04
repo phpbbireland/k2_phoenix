a13acb6a32f04fa56362e097f47229361ebd1045
Michael O'Toole <phpbbireland@gmail.com>
2012-07-04 23:33:24 +0100

    More 1.0.16 I missed... part II
    Should be all of them...

--- root/portal/index.php
+++ root/portal/index.php
@@ -4,7 +4,7 @@
 * @author michaelo phpbbireland@gmail.com - http://www.phpbbireland.com
 *
 * @package sgp
-* @version 1.0.15
+* @version 1.0.16
 * @copyright (c) 2005-2011 Michael O'Toole (phpbbireland.com)
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
@@ -85,8 +85,8 @@ $versions = array(
 	// Version 1.0.0n
 	'1.0.0n' => array(
 	),
-	// Version 1.0.15
-	'1.0.15'	=> array(
+	// Version 1.0.16
+	'1.0.16'	=> array(
 		'permission_add' => array(
 			array('a_k_portal', 1),
 			array('a_k_tools', 1),
@@ -115,23 +115,23 @@ $versions = array(
 		'table_add' => array(
 			array('phpbb_k_blocks', array(
 					'COLUMNS' => array(
-						'id'			=> array('UINT', NULL, 'auto_increment'),
-						'ndx'			=> array('UINT', '0'),
-						'title'			=> array('VCHAR:50', ''),
-						'position'		=> array('CHAR:1', 'L'),
-						'type'			=> array('CHAR:1', 'H'),
-						'active'		=> array('BOOL', '1'),
+						'id'				=> array('UINT', NULL, 'auto_increment'),
+						'ndx'				=> array('UINT', '0'),
+						'title'				=> array('VCHAR:50', ''),
+						'position'			=> array('CHAR:1', 'L'),
+						'type'				=> array('CHAR:1', 'H'),
+						'active'			=> array('BOOL', '1'),
 						'html_file_name'	=> array('VCHAR', ''),
 						'var_file_name'		=> array('VCHAR', 'none.gif'),
 						'img_file_name'		=> array('VCHAR', 'none.gif'),
-						'view_all'		=> array('BOOL', '1'),
+						'view_all'			=> array('BOOL', '1'),
 						'view_groups'		=> array('VCHAR:100', ''),
 						'view_pages'		=> array('VCHAR:100', ''),
-						'groups'		=> array('UINT', '0'),
-						'scroll'		=> array('BOOL', '0'),
+						'groups'			=> array('UINT', '0'),
+						'scroll'			=> array('BOOL', '0'),
 						'block_height'		=> array('USINT', '0'),
-						'has_vars'		=> array('BOOL', '0'),
-						'is_static'		=> array('BOOL', '0'),
+						'has_vars'			=> array('BOOL', '0'),
+						'is_static'			=> array('BOOL', '0'),
 						'minimod_based'		=> array('BOOL', '0'),
 						'mod_block_id'		=> array('UINT', '0'),
 						'block_cache_time'	=> array('UINT', '600'),
@@ -142,18 +142,18 @@ $versions = array(
 
 			array('phpbb_k_menus', array(
 					'COLUMNS'	=> array(
-						'm_id'		=> array('UINT', NULL, 'auto_increment'),
-						'ndx'		=> array('UINT', '0'),
-						'menu_type'	=> array('USINT', '0'),
-						'name'		=> array('VCHAR:50', ''),
-						'link_to'	=> array('VCHAR', ''),
-						'extern'	=> array('BOOL', '0'),
-						'menu_icon'	=> array('VCHAR:30', 'none.gif'),
+						'm_id'			=> array('UINT', NULL, 'auto_increment'),
+						'ndx'			=> array('UINT', '0'),
+						'menu_type'		=> array('USINT', '0'),
+						'name'			=> array('VCHAR:50', ''),
+						'link_to'		=> array('VCHAR', ''),
+						'extern'		=> array('BOOL', '0'),
+						'menu_icon'		=> array('VCHAR:30', 'none.gif'),
 						'append_sid'	=> array('BOOL', '1'),
 						'append_uid'	=> array('BOOL', '0'),
-						'view_all'	=> array('BOOL', '1'),
+						'view_all'		=> array('BOOL', '1'),
 						'view_groups'	=> array('VCHAR:100', ''),
-						'soft_hr'	=> array('BOOL', '0'),
+						'soft_hr'		=> array('BOOL', '0'),
 						'sub_heading'	=> array('BOOL', '0'),
 					),
 					'PRIMARY_KEY'	=> 'm_id',
@@ -162,11 +162,11 @@ $versions = array(
 
 			array('phpbb_k_blocks_config', array(
 					'COLUMNS'	=> array(
-						'id'			=> array('USINT', NULL, 'auto_increment'),
+						'id'					=> array('USINT', NULL, 'auto_increment'),
 						'use_external_files'	=> array('BOOL', '0'),
-						'update_files'		=> array('BOOL', '0'),
-						'layout_default'	=> array('BOOL', '2'),
-						'portal_config'		=> array('VCHAR:10', 'Site'),
+						'update_files'			=> array('BOOL', '0'),
+						'layout_default'		=> array('BOOL', '2'),
+						'portal_config'			=> array('VCHAR:10', 'Site'),
 					),
 				'PRIMARY_KEY'	=> 'id',
 				),
@@ -242,7 +242,7 @@ $versions = array(
 			array('ucp', '0', 'UCP_K_BLOCKS'),
 			array('ucp', 'UCP_K_BLOCKS', array(
 					'module_basename'	=> 'k_blocks',
-					'modes'			=> array('info', 'arrange', 'edit', 'delete', 'width'),
+					'modes'				=> array('info', 'arrange', 'edit', 'delete', 'width'),
 					'module_auth'		=> 'u_k_tools',
 				),
 			),
